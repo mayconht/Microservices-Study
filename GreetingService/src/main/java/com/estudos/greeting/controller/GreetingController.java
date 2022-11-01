09b625532b581e5b688b285f45b459863ae2e394
Maycon Douglas <maycondss@live.com>
2022-10-31 22:58:10 -0300

    Estudos Microservice

---
+++ GreetingService/src/main/java/com/estudos/greeting/controller/GreetingController.java
@@ -0,0 +1,23 @@
+package com.estudos.greeting.controller;
+
+import com.estudos.greeting.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") final String name) {
+        return new Greeting(
+                this.counter.incrementAndGet(),
+                String.format(template, name)
+        );
+    }
+}
