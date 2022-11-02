f414553ed478c91908bd1c9a406850c52c6e7d82
Maycon Douglas <maycondss@live.com>
2022-11-02 14:51:59 -0300

    Updating Controller

--- GreetingService/src/main/java/com/estudos/greeting/controller/GreetingController.java
+++ GreetingService/src/main/java/com/estudos/greeting/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package com.estudos.greeting.controller;
 
+import com.estudos.greeting.configuration.GreetingConfiguration;
 import com.estudos.greeting.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +12,23 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "%s, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    private GreetingConfiguration greetingConfiguration;
+
+
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") final String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "") String name) {
+
+        if (name.isEmpty()) {
+            name = this.greetingConfiguration.getDefaultName();
+        }
+
         return new Greeting(
                 this.counter.incrementAndGet(),
-                String.format(template, name)
+                String.format(template, this.greetingConfiguration.getGreeting(), name)
         );
     }
 }
