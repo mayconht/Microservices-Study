fdfc60cbf79f841d21f438d677a1ccde55b4218d
Maycon Douglas <maycondss@live.com>
2022-11-11 16:27:16 -0300

    Environment

--- cambio/src/main/java/br/com/cambio/controller/CambioController.java
+++ cambio/src/main/java/br/com/cambio/controller/CambioController.java
@@ -1,6 +1,8 @@
 package br.com.cambio.controller;
 
 import br.com.cambio.model.Cambio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +13,17 @@ import java.math.BigDecimal;
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
+    
+    @Autowired
+    Environment env;
 
     //    http://localhost:8000/cambio-service/5/USD/BRL
     @GetMapping(value = "/{amount}/{from}/{to}")
-    public static Cambio getCambio(@PathVariable("amount") final BigDecimal amount,
-                                   @PathVariable("from") final String from,
-                                   @PathVariable("to") final String to) {
-        
-        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+    public Cambio getCambio(@PathVariable("amount") final BigDecimal amount,
+                            @PathVariable("from") final String from,
+                            @PathVariable("to") final String to) {
+
+        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, this.env.getProperty("local.server.port"));
     }
 
 }
